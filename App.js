fb60bc32e3d119542666f2ec11f183a0e0593ced
bsao17 <declic62@gmail.com>
2021-12-31 12:58:46 +0100

    update reducer toggleFavorites

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import * as React from "react";
 import Navigation from "./navigation/Navigation";
 import { Provider } from "react-redux";
-import moviesReducer from "./store/reducer/moviesReducer";
+import toggleFavorites from "./store/reducer/moviesReducer";
 import {createStore} from "redux";
 
-const store = createStore(moviesReducer);
+const store = createStore(toggleFavorites);
 
 export default function App() {
   return (
--- store/reducer/moviesReducer.js
+++ store/reducer/moviesReducer.js
@@ -1,19 +1,19 @@
 import { ADD_TO_FAVORITES } from "../action/moviesAction";
 import { REMOVE_FROM_FAVORITES } from "../action/moviesAction";
 
-export default function moviesReducer(state = [], action) {
-    let nextState;
+export default function toggleFavorites(state = [], action) {
+  let nextState;
   switch (action.type) {
     case ADD_TO_FAVORITES:
       nextState = {
         ...state,
         movies: [...state.push(action.movies)],
       };
-      case REMOVE_FROM_FAVORITES:
-        nextState = {
-            ...state,
-            movies: [...state.filter(movies => movies.id !== action.movies.id)],
-        };
+    case REMOVE_FROM_FAVORITES:
+      nextState = {
+        ...state,
+        movies: [...state.filter((movies) => movies.id !== action.movies.id)],
+      };
       return nextState;
     default:
       return state;
